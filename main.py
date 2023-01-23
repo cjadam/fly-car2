c3f89ef694295e90e8914e00ad4144d350bcdd1e
cjadam <103041160+cjadam@users.noreply.github.com>
2023-01-23 14:00:06 +0800

    更新 pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
 basic.show_icon(IconNames.HEART)
 music.play_sound_effect(music.builtin_sound_effect(soundExpression.spring),
     SoundExpressionPlayMode.UNTIL_DONE)
-music.start_melody(music.built_in_melody(Melodies.ODE),
+music.set_volume(62)
+music.start_melody(music.built_in_melody(Melodies.BIRTHDAY),
     MelodyOptions.FOREVER_IN_BACKGROUND)
 
 def on_forever():
-    if input.is_gesture(Gesture.LOGO_UP) == False or input.is_gesture(Gesture.SHAKE) == True:
-        SuperBitV2.motor_stop_all()
+    if SuperBitV2_Digital.ultrasonic(SuperBitV2_Digital.mwDigitalNum.P10P9) < 32:
+        basic.show_arrow(ArrowNames.SOUTH)
+        SuperBitV2.motor_run_dual(SuperBitV2.enMotors.M1, -119, SuperBitV2.enMotors.M3, -144)
+        basic.pause(200)
+        SuperBitV2.motor_run_dual(SuperBitV2.enMotors.M1, -132, SuperBitV2.enMotors.M3, 132)
     else:
-        if SuperBitV2_Digital.ultrasonic(SuperBitV2_Digital.mwDigitalNum.P10P9) < 30 or SuperBitV2_Digital.IR(SuperBitV2_Digital.mwDigitalNum.P4P6,
-            SuperBitV2_Digital.enObstacle.OBSTACLE):
-            basic.show_arrow(ArrowNames.SOUTH)
-            SuperBitV2.motor_run_dual(SuperBitV2.enMotors.M1, -119, SuperBitV2.enMotors.M3, -144)
-        else:
-            basic.show_arrow(ArrowNames.NORTH)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.RED))
-            SuperBit.RGB_Program().show()
-            SuperBit.motor_run(SuperBit.enMotors.M1, 255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, 255)
-            basic.pause(2000)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.GREEN))
-            SuperBit.motor_run(SuperBit.enMotors.M1, -255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, -255)
-            basic.pause(2000)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.BLUE))
-            SuperBit.motor_run(SuperBit.enMotors.M1, -255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, 255)
-            basic.pause(1000)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.VIOLET))
-            SuperBit.motor_run(SuperBit.enMotors.M1, 255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, -255)
-            basic.pause(1000)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.RED))
-            SuperBit.motor_run(SuperBit.enMotors.M1, -255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, 255)
-            basic.pause(1000)
-            SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.GREEN))
-            SuperBit.motor_run(SuperBit.enMotors.M1, 255)
-            SuperBit.motor_run(SuperBit.enMotors.M3, -255)
-            basic.pause(1000)
+        SuperBit.RGB_Program().show_color(neopixel.colors(NeoPixelColors.PURPLE))
+        SuperBit.RGB_Program().show()
+        basic.show_arrow(ArrowNames.NORTH)
+        SuperBitV2.motor_run_dual(SuperBitV2.enMotors.M1, 169, SuperBitV2.enMotors.M3, 193)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,31 +2,17 @@ basic.showIcon(IconNames.Heart)
 music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
 music.setVolume(62)
 music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.ForeverInBackground)
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (SuperBitV2_Digital.Ultrasonic(SuperBitV2_Digital.mwDigitalNum.P10P9) < 32) {
         basic.showArrow(ArrowNames.South)
-        SuperBitV2.MotorRunDual(
-        SuperBitV2.enMotors.M1,
-        -119,
-        SuperBitV2.enMotors.M3,
-        -144
-        )
+        SuperBitV2.MotorRunDual(SuperBitV2.enMotors.M1, -119, SuperBitV2.enMotors.M3, -144)
         basic.pause(200)
-        SuperBitV2.MotorRunDual(
-        SuperBitV2.enMotors.M1,
-        -132,
-        SuperBitV2.enMotors.M3,
-        132
-        )
+        SuperBitV2.MotorRunDual(SuperBitV2.enMotors.M1, -132, SuperBitV2.enMotors.M3, 132)
     } else {
         SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
         SuperBit.RGB_Program().show()
         basic.showArrow(ArrowNames.North)
-        SuperBitV2.MotorRunDual(
-        SuperBitV2.enMotors.M1,
-        169,
-        SuperBitV2.enMotors.M3,
-        193
-        )
+        SuperBitV2.MotorRunDual(SuperBitV2.enMotors.M1, 169, SuperBitV2.enMotors.M3, 193)
     }
+    
 })
